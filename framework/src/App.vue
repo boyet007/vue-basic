fb98900e82d05bc2977a3d76693c83e0bfa482a0
Philip Loa <boyet007@gmail.com>
2018-08-19 15:22:43 +0700

    tutorial 09 Vue Files and root component

--- framework/src/App.vue
+++ framework/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ greeting() }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  data() {
+    return {
+      title: "Your first vue file..whooo"
+    };
+  },
+  methods: {
+    greeting: function() {
+      return "heyy coboy";
+    }
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
